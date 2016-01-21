a4ff70cb238f4a82693c55684ab3d15646a7bfe8
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-20 16:28:49 -1000

    finish

--- indexing.sql
+++ indexing.sql
@@ -13,25 +13,50 @@ CREATE DATABASE indexed_cars;
 
 \i Automotive-Index/scripts/car_model_data.sql
 
-\timing
+--\timing
 
-SELECT DISTINCT make_title
-FROM car_models
-WHERE make_code = 'LAM';
+-- --1
+-- SELECT DISTINCT make_title
+-- FROM car_models
+-- WHERE make_code = 'LAM';
 
 -- this enables the timing thing
 -- it shows you how long it takes for the process to finish
 
 -- DISTINCT make sure there are no duplicates
 
-SELECT DISTINCT make_title
-FROM car_models
-WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+-- --2
+-- SELECT DISTINCT make_title,
+-- FROM car_models
+-- WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+
+
+-- --3
+-- SELECT make_code, model_code, model_title, year
+-- FROM car_models
+-- WHERE make_code = 'LAM';
+
+-- -- --4
+-- SELECT count(*)
+-- FROM car_models
+-- WHERE year BETWEEN 2010 AND 2015;
+q
+-- --5
+-- SELECT count(*)
+-- FROM car_models
+-- WHERE year = 2010;
+
+CREATE INDEX get_make_code
+  ON car_models (make_code);
+
+CREATE INDEX get_model_title
+  ON car_models (make_code, model_code);
+
+CREATE INDEX get_car_year
+  ON car_models (year)
 
 
 
-
-
-\dt
+--\dt
 
 -- SELECT * FROM car_models;
